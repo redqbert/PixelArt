346fc221a6d0ffc565e7567878da575385fe2ee7
Alvaro-GF <granadosalvaro09@gmail.com>
2024-05-24 18:42:28 -0600

    Prueba

---
+++ Practica.py
@@ -0,0 +1,20 @@
+class pincel:
+    brocha = "brocha"
+    borrador = False
+    zoom_in = False
+    zoom_out = False
+    def __init__(self, brocha, borrador, zoom_in, zoom_out):
+        self.brocha = brocha
+        self.borrador =  borrador
+        self.zoom_in = zoom_in
+        self.zoom_out = zoom_out
+
+    def interruptor_borrar(self):
+        self.borrador = not self.borrador
+        if self.borrador == False:
+            brocha = "Brocha"
+        else:
+            brocha = "Borrador"
+
+    def cambio_brocha(self):
+
