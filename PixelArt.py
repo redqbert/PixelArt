7877dd9853d76cd91634ba29ac6bca1c22b1c455
Redqbert <isaacvg333@gmail.com>
2024-05-27 15:20:20 -0600

    Creacion de colores y pintar sobre la matriz,junto con creacion de matriz numerica,asignacion de ids

--- PixelArt.py
+++ PixelArt.py
@@ -5,41 +5,55 @@ pygame.init()
 
 
 
+
+# Area variables globales
+tamaño_cuadros = 32 #Pixeles c/u
+comienzo_dibujar_cuadros = tamaño_cuadros * 3 #Lugar donde se empiezan a generar los cuadros
+rect_raton = pygame.Rect(0,0,10,10)
+
+gris = (220,220,220)
+
+estado = 'pintar en lienzo' #Trabajarlo por estados para mas comodidad
+
+
+
 # Area para funciones 
 
-
 #Funcion de matriz
-
 
 #La matriz sera creada con cada subconjuto lleno de ceros, pero sientete libre de cambiar el 0 por otro valor, como ""
 #Cambie el orden de donde se colocan las funciones, ya que de lo contrario estas quedan indefinidas
 
 def generador_matriz():
     matriz = []
-    for y in range(17):
+    matriz_rects=[]
+    for fila in range(0,17):
         matriz.append([])
-        for x in range(26):
-            matriz_subgrupo = matriz[y]
-            matriz_subgrupo.append(0) #Este es el relleno de todos los elementos
-    return matriz
+        for columna in range(0,26):
+            matriz[fila].append(0) #Este es el relleno de todos los elementos de la matriz de 0
+            rect = pygame.Rect((tamaño_cuadros+0.2)*fila+comienzo_dibujar_cuadros,
+                               (tamaño_cuadros+0.1)*columna+comienzo_dibujar_cuadros, 
+                               tamaño_cuadros*0.92, 
+                               tamaño_cuadros*0.92) 
+            matriz_rects.append(rect) #Rectangulos del canvas
+    return matriz,matriz_rects
+
 
 
 # Clases
 
-
 class Editor():
     # Atributos
-    matriz = generador_matriz() #Matriz
+    matriz,matriz_rects = generador_matriz() #Matriz
     creador = ""
     estado_programa = ""
     estilo_imagen = ""
     #Falta agregar metodos
 
-    def __init__(self, matriz, creador, estado_programa, estilo_imagen):
-        self.matriz = matriz
+    def __init__(self, creador, estado_programa, estilo_imagen):
         self.creador = creador
         self.estado_programa = "Creado"
-        estilo_imagen = estilo_imagen
+        self.estilo_imagen = estilo_imagen
 
     def estado_en_ejecucion(self):
         if self.estado_programa == "Creado":
@@ -51,13 +65,19 @@ class Editor():
         elif self.estado_programa == "Terminado":
             pass
     
-    def modificar_matriz(self, localizacion, valor): #Cambia un valor de la matriz
+    def editar_imagen(self, localizacion, valor): #Cambia un valor de la matriz
         x, y = localizacion
         self.matriz[y][x] = valor
-
-
-
-class pincel:
+###
+#
+#
+#
+# Clases van con nombre mayuscula
+#
+#
+#
+##
+class Pincel:
     brocha = "brocha"
     borrador = False
     zoom_in = False
@@ -78,14 +98,27 @@ class pincel:
     def cambio_brocha(self):
         pass
 
-# Area variables
-tamaño_cuadros = 32 #Pixeles c/u
-comienzo_dibujar_cuadros = 32 * 3 #Lugar donde se empiezan a generar los cuadros
-rect_raton = pygame.Rect(0,0,10,10)
-rect_raton.centerx = 400 #Cuadro que seguira al cursor
+class Color:
+    imagen=''
+    tamaño_imagen=0
+    id=''
+    posicion_x=0
+    posicion_y=0
+    def __init__(self, imagen, id, tamaño_imagen,posicion_x,posicion_y):
+        self.imagen = pygame.image.load(imagen)
+        self.id = id
+        self.tamaño_imagen = tamaño_imagen
+        self.imagen = pygame.transform.scale(self.imagen,(tamaño_imagen*tamaño_cuadros,tamaño_imagen*tamaño_cuadros))
+        self.rect = self.imagen.get_rect()
+        self.posicion_x = posicion_x * tamaño_cuadros
+        self.posicion_y = posicion_y * tamaño_cuadros
 
-rect1 = pygame.Rect(0,0,tamaño_cuadros,tamaño_cuadros) #Rectangulo del canvas
-color_rect1 = (220,220,220) #Color del rectangulo
+    def generar_cuadro(self):
+        pantalla.blit(self.imagen,(self.posicion_x,self.posicion_y ))
+        
+        
+
+
 
 # pygame setup
 pantalla = pygame.display.set_mode((1100, 700)) #Reduje el tamaño del cuadro de (1600, 900) ---> (1100, 700), si afecta mucho la escala de tamaños aumentalo nuevamente
@@ -93,56 +126,35 @@ clock = pygame.time.Clock()
 
 # Variable para determinar cuando se cierra la ventana
 jugar = True
+# Iniciar objetos
+lienzo = Editor("", "Default", "Default")
+
+colores = [['amarillo',1,2,1,2 ],['celeste',1,2,1,3],['gris',1,2,1,4],['morado',1,2,1,5],['naranja',1,2,1,6],['rojo',1,2,1,7]
+           ,['rosado',1,2,1,8],['verde',1,2,1,9]]
+
+objetos_colores=[]
+for fila in colores:
+    color = Color(f'colores/{fila[0]}.png',fila[1],fila[2],fila[3],fila[4]) #Colores
+    objetos_colores.append(color)
+
 
 
 
 while jugar:
 
-    lienzo = Editor("", "Default", "Default" )
-
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             jugar = False
 
-
-        #Esta funcion analiza en cual cuadro ocurrio la interaccion
-        def analizador_de_bloques(): 
-            localizacion_interaccion = []
-            espacio_ventana_x = 0 #Aqui va el espacio que esta entre el lienzo y la ventana en el eje x
-            espacio_ventana_y = 0 #Aqui va el espacio que esta entre el lienzo y la ventana en el eje y
-            (ratonx, ratony) = pygame.mouse.get_pos()
-            matriz = lienzo.matriz()
-            for grupo_y in range(len(matriz)):
-                for unidad_x in range(len(grupo_y)):
-                    #La condicion de la siguiente linea verifica si la posicion del cursor esta en el rango efectivo del cuadro al que se quiere afectar
-                    if (ratonx < ((unidad_x+1)*32) + espacio_ventana_x) and (ratonx > ((unidad_x*32) + espacio_ventana_x)) and (ratony < ((grupo_y+1)*32) + espacio_ventana_y) and (ratony > (grupo_y*32) + espacio_ventana_y ):
-                        localizacion_interaccion = [unidad_x,grupo_y]
-                    else:
-                        continue
-            return localizacion_interaccion
-
-        #Analiza si la interacicon fue en el lienzo
-        def zona_efectiva(): 
-            zona_canvas_x = 0 #limite del eje x en el que se puede encontrar un cuadro, desconozco su tamaño con exactitud
-            zona_canvas_y = 0 #limite del eje y en el que se puede encontrar un cuadro, desconozco su tamaño con exactitud
-            (ratonx, ratony) = pygame.mouse.get_pos() 
-            if ratonx > zona_canvas_x and ratony > zona_canvas_y:
-                return True
-            else:
-                return False
+        if event.type == pygame.MOUSEBUTTONDOWN: 
+            for elemento in  lienzo.matriz_rects :
+                    if rect_raton.colliderect(elemento):
+                        pygame.draw.rect(pantalla,(0,220,220),elemento)#pinta del color(0,220,220)
+                        lienzo.editar_imagen
 
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            interaccion = zona_efectiva()
-            if interaccion == True: #Comprueba si el click fue en el lienzo
-                lugar_afectado = analizador_de_bloques()
-                lienzo.modificar_matriz(analizador_de_bloques, )#Falta agregar codigo
-
-
-
-    # Refrescar la pantalla por cada frame para quitar el frame anterior
-    pantalla.fill("white")
+    for elemento in objetos_colores:
+        elemento.generar_cuadro()
 
     # Logica
     posicion_raton = pygame.mouse.get_pos()
@@ -152,19 +164,18 @@ while jugar:
     #La idea es que se cargue solo una vez al inicio,luego cuando detecte el mouse,hara que se vuelva a cargar si fuese que se selecciono un color
     
     #Generador de cuadros
+    if estado == 'pintar en lienzo':
+        pantalla.fill("white")
+        print(lienzo.matriz)
 
-    for i in range(26):     #Numero de cuadros en el eje x
-        for j in range(17):     # Numero de cuadros en el ejey
-            if i == 2 and j == 3:   #Porque esta condicion?
-                # parametros del draw.rect: superficie,color,(posicion  )
-                pygame.draw.rect(pantalla,(220,220,220),(tamaño_cuadros*i+comienzo_dibujar_cuadros,tamaño_cuadros*j+comienzo_dibujar_cuadros,tamaño_cuadros,tamaño_cuadros))
-            else:
-                pygame.draw.rect(pantalla,(220,220,220),(tamaño_cuadros*i+comienzo_dibujar_cuadros,tamaño_cuadros*j+comienzo_dibujar_cuadros,tamaño_cuadros,tamaño_cuadros),3)
+        for fila in range(0, len(lienzo.matriz ) ):     #Numero de cuadros en el eje x
+            for columna in range(0,len(lienzo.matriz) ):     # Numero de cuadros en el ejey
+                pygame.draw.rect(pantalla,(220,220,220),(tamaño_cuadros*fila+comienzo_dibujar_cuadros,tamaño_cuadros*columna+comienzo_dibujar_cuadros,tamaño_cuadros,tamaño_cuadros),2)
+       
+        estado = 'siguiente'
 
-    
-    pygame.draw.rect(pantalla,(0,255,255),rect_raton,3)#Se cambia al final para que el color sea blanco,es para la deteccion de colision entre el rato y un cuadro
-    pygame.draw.rect(pantalla,color_rect1,rect1)#Se cambia al final para que el color sea blanco,es para la deteccion de colision entre el rato y un cuadro
-
+    #pygame.draw.rect(pantalla,(0,255,255),rect_raton,3)#Se cambia al final para que el color sea blanco,es para la deteccion de colision entre el rato y un cuadro
+    #pygame.draw.rect(pantalla,color_rect1,rect1,2)#Se cambia al final para que el color sea blanco,es para la deteccion de colision entre el rato y un cuadro
     # Colocar en la pantalla el renderizado
     pygame.display.flip()    
 
@@ -174,4 +185,3 @@ while jugar:
 
 
 pygame.quit()
-
